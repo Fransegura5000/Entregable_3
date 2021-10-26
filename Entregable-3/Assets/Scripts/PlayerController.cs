23ae9d2dbd4434a54704b75567b33655f0a9361f
Fransegura5000 <fseguraladat@gmail.com>
2021-10-26 10:45:07 +0200

    Francisco Segura Urbano Entregable 3

---
+++ Entregable-3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ScaleGameObject(new Vector3(0.5f, 0, 0), KeyCode.X);
+        ScaleGameObject(new Vector3(0, 0.5f, 0), KeyCode.Y); 
+        ScaleGameObject(new Vector3(0, 0, 0.5f), KeyCode.Z);
+
+
+    }
+    public void ScaleGameObject(Vector3 scale, KeyCode keyCode)
+
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            transform.localScale += scale;
+        }
+
+    }
+
+
+
+
+
+
+
+}
